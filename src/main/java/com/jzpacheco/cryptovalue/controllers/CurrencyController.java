6f2e490b9de85f87e7382a4eb86156dc63a72620
jzpacheco <jeffersonpacheco@hotmail.com>
2024-04-21 20:10:18 -0300

    Created currency controller

--- src/main/java/com/jzpacheco/cryptovalue/controllers/CurrencyController.java
+++ src/main/java/com/jzpacheco/cryptovalue/controllers/CurrencyController.java
@@ -1,9 +1,12 @@
 package com.jzpacheco.cryptovalue.controllers;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jzpacheco.cryptovalue.domains.currency.Currency;
+import com.jzpacheco.cryptovalue.services.CurrencyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -11,7 +14,28 @@ import java.util.List;
 @RequestMapping("/api/currency/v1/")
 public class CurrencyController {
 
+    @Autowired
+    private CurrencyService service;
 
     @GetMapping
-    public List<Currency>
+    public ResponseEntity<List<Currency>> getAllCurrencies(){
+        try {
+            List<Currency> currencies = service.getAllCurrencies();
+            return ResponseEntity.ok(currencies);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping("/addCurrency")
+    public ResponseEntity<Currency> addCurrency(@RequestParam String name){
+        try{
+            System.out.println(name);
+            Currency currency =  service.addCurrency(name);
+            return ResponseEntity.ok(currency);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+    }
 }
